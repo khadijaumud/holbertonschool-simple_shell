13f7eef68ca22e9889ce53c23122004e65766c75
niceehi <hsnislam2026@gmail.com>
2026-02-18 02:39:17 +0400

    shell

--- shell.h
+++ shell.h
@@ -13,8 +13,8 @@ extern char **environ;
 void prompt(void);
 char **parse_line(char *line);
 int execute_cmd(char **argv);
-void free_args(char **argv);
 char *_which(char *command);
 char *_getenv(const char *name);
 
 #endif
+
