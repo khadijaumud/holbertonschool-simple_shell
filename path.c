31c1d5db8a9caf44d1ef0c78b47ca0f704ed59b9
niceehi <hsnislam2026@gmail.com>
2026-02-18 02:38:52 +0400

    path

--- path.c
+++ path.c
@@ -1,38 +1,30 @@
 #include "shell.h"
 
 /**
- * _getenv - gets the value of an environment variable
- * @name: name of the variable
- * Return: pointer to the value, or NULL if not found
+ * _getenv - gets an environment variable without using standard getenv
  */
 char *_getenv(const char *name)
 {
 	int i = 0;
-	size_t name_len = strlen(name);
+	size_t len = strlen(name);
 
 	while (environ[i])
 	{
-		if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
-			return (environ[i] + name_len + 1);
+		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+			return (environ[i] + len + 1);
 		i++;
 	}
 	return (NULL);
 }
 
 /**
- * _which - locates a command in the PATH directories
- * @command: the command name
- * Return: full path string if found, NULL otherwise
+ * _which - finds a command in PATH
  */
 char *_which(char *command)
 {
-	char *path, *path_copy, *token, *full_path;
+	char *path, *path_cp, *token, *full_path;
 	struct stat st;
 
-	if (!command)
-		return (NULL);
-
-	/* ПЕРВАЯ ПРОВЕРКА: Если путь уже полный или относительный */
 	if (strchr(command, '/'))
 	{
 		if (stat(command, &st) == 0)
@@ -44,28 +36,21 @@ char *_which(char *command)
 	if (!path || *path == '\0')
 		return (NULL);
 
-	path_copy = strdup(path);
-	token = strtok(path_copy, ":");
+	path_cp = strdup(path);
+	token = strtok(path_cp, ":");
 	while (token)
 	{
 		full_path = malloc(strlen(token) + strlen(command) + 2);
-		if (full_path)
+		sprintf(full_path, "%s/%s", token, command);
+		if (stat(full_path, &st) == 0)
 		{
-			sprintf(full_path, "%s/%s", token, command);
-			if (stat(full_path, &st) == 0)
-			{
-				free(path_copy);
-				return (full_path);
-			}
-			free(full_path);
+			free(path_cp);
+			return (full_path);
 		}
+		free(full_path);
 		token = strtok(NULL, ":");
 	}
-	free(path_copy);
-	return (NULL);	
+	free(path_cp);
+	return (NULL);
 }
-
-
-
-
 
