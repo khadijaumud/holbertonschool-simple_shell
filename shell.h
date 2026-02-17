f22bf903a1dc15271b35c7ac0ab892a1fc705243
khadijaumud <kh.umud07@gmail.com>
2026-02-17 17:25:58 +0000

    update shell.h file

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+extern char **environ;
+
+void prompt(void);
+char **parse_line(char *line);
+void execute_cmd(char **argv);
+void free_args(char **argv);
+
+#endif
