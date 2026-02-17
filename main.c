94f1610a4a66303044e622a6a564664403c6aad7
niceehi <hsnislam2026@gmail.com>
2026-02-18 02:38:24 +0400

    Refactor main function and clean up code

--- main.c
+++ main.c
@@ -11,9 +11,6 @@ void prompt(void)
 
 /**
  * parse_line - splits input into arguments
- * @line: user input
- *
- * Return: array of arguments
  */
 char **parse_line(char *line)
 {
@@ -22,7 +19,7 @@ char **parse_line(char *line)
 	int i = 0;
 
 	argv = malloc(sizeof(char *) * 64);
-	if (argv == NULL)
+	if (!argv)
 		return (NULL);
 
 	token = strtok(line, " \t\r\n");
@@ -32,61 +29,11 @@ char **parse_line(char *line)
 		token = strtok(NULL, " \t\r\n");
 	}
 	argv[i] = NULL;
-
 	return (argv);
 }
 
-
 /**
- * free_args - frees argument array
- * @argv: argument array
- */
-void free_args(char **argv)
-{
-	free(argv);
-}
-
-/**
- * main - simple shell entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char **argv;
-	int last_status = 0;
-
-	while (1)
-	{
-		prompt(); [cite: 1]
-
-		read = getline(&line, &len, stdin); [cite: 1]
-		if (read == -1)
-		{
-			free(line);
-			exit(last_status);
-		}
-
-		argv = parse_line(line); [cite: 1]
-		if (argv == NULL || argv[0] == NULL)
-		{
-			free(argv);
-			continue;
-		}
-
-		last_status = execute_cmd(argv);
-		free_args(argv); [cite: 1]
-	}
-	return (last_status);
-}
-
-/**
- * execute_cmd - executes command only if it exists in PATH
- * @argv: arguments array
- * Return: status code (0 for success, 127 for not found)
+ * execute_cmd - executes command and returns exit status
  */
 int execute_cmd(char **argv)
 {
@@ -97,12 +44,11 @@ int execute_cmd(char **argv)
 	if (argv == NULL || argv[0] == NULL)
 		return (0);
 
-	full_path = _which(argv[0]); [cite: 1, 3]
+	full_path = _which(argv[0]);
 	if (full_path == NULL)
 	{
-		/* Формат ошибки: shell_name: line_number: command: not found */
 		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-		return (127); /* Ожидаемый код для "command not found" */
+		return (127);
 	}
 
 	pid = fork();
@@ -121,10 +67,34 @@ int execute_cmd(char **argv)
 		if (WIFEXITED(status))
 			exit_status = WEXITSTATUS(status);
 	}
-	else
-		perror("fork");
-
 	free(full_path);
 	return (exit_status);
+}
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n_read;
+	char **argv;
+	int last_status = 0;
+
+	while (1)
+	{
+		prompt();
+		n_read = getline(&line, &len, stdin);
+		if (n_read == -1)
+		{
+			free(line);
+			exit(last_status);
+		}
+
+		argv = parse_line(line);
+		if (argv && argv[0])
+			last_status = execute_cmd(argv);
+
+		free(argv);
+	}
+	return (last_status);
 }
 
